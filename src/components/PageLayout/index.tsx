84ab8c7f4ad8c250a83c0f721bacd9e9b8cf63dc
Hong <hongchan88@hotmail.com>
2024-11-02 22:53:37 +0700

    sidebar minimize sidebar

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,14 +1,10 @@
-import React, {
-  MutableRefObject,
-  RefObject,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { RefObject, useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
-import { useReactToPrint } from 'react-to-print';
 import { useDataStore } from '../../stores/dataStore';
-
+import {
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from '@radix-ui/react-icons';
 interface PageLayoutProps {
   children: React.ReactNode;
   Form: React.ComponentType;
@@ -30,14 +26,27 @@ const PageLayout: React.FC<PageLayoutProps> = ({ Form, children }) => {
     <div className='flex w-full min-h-screen'>
       <div
         className={`bg-gray-100 rounded-r-md fixed h-screen transition-all duration-300 ease-in-out ${
-          isSidebarOpen ? 'w-74' : 'w-16'
+          isSidebarOpen ? 'w-74' : 'w-13'
         } ${styles.form}`}
       >
+        <div className='flex justify-end p-3'>
+          {isSidebarOpen ? (
+            <DoubleArrowLeftIcon
+              onClick={() => setIsSidebarOpen((prev) => !prev)}
+              className='w-7 h-7 cursor-pointer'
+            />
+          ) : (
+            <DoubleArrowRightIcon
+              onClick={() => setIsSidebarOpen((prev) => !prev)}
+              className='w-7 h-7 cursor-pointer'
+            />
+          )}
+        </div>
         {isSidebarOpen ? <Form /> : null}
       </div>
       <div
         ref={contentRef}
-        className={`${isSidebarOpen ? 'ml-64' : ''}  ${styles.page} `}
+        className={` ${styles.page} ${!isSidebarOpen ? styles.minimize : ''} `}
       >
         {children}
       </div>
